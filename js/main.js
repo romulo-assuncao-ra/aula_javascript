229ec62721499d3626a846f0812f9e435a414ca3
Rômulo Assunção Corrêa <romulo.assuncao.ra@gmail.com>
2023-01-29 20:58:33 -0400

    introdução ao javascript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var lista = ["maça","pera","laranja"];
+console.log(lista[1]);
+alert(lista[1]);
+
+/*var nome = "Romulo";
+var idade = 29;
+var idade2 = 10;
+
+alert(nome + " tem " + idade + " anos ");
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+alert(frase.replace("japão", "Brasil"));*/
